7a871d43a6dcc63e128fbe6a9ce2222d997324bc
Dallas <dallas.oliver91@gmail.com>
2019-07-07 23:01:53 -0400

    working more on trying to send the correct response to the client

--- index.js
+++ index.js
@@ -3,46 +3,42 @@ const config = require("./config");
 const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
+const base64url = require("base64url");
+global.fetch = fetch;
 const T = new Twit(config.twit);
 
 const Unsplash = require("unsplash-js").default;
 const unsplash = new Unsplash(config.unsplash);
 
-const authenticationUrl = unsplash.auth.getAuthenticationUrl([
-  "public",
-  "read_user",
-  "write_user",
-  "read_photos",
-  "write_photos"
-]);
-
-location.assign(authenticationUrl);
-
-unsplash.auth
-  .userAuthentication(query.code)
-  .then(toJson)
-  .then(json => {
-    unsplash.auth.setBearerToken(json.access_token);
-    console.log(json.access_token);
-  });
-
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/request-word/:word", async (req, res) => {
-  console.log(req.params);
-  const MW_API_KEY = "7198bafe-4e1d-43b0-92d8-163a1647cb08";
-  const MW_url = `https://dictionaryapi.com/api/v3/references/collegiate/json/${
-    req.params.word
-  }?key=${MW_API_KEY}`;
-  const MW_response = await fetch(MW_url);
-  const MW_data = await MW_response.json();
+  // console.log(req.params);
+  // const MW_API_KEY = "7198bafe-4e1d-43b0-92d8-163a1647cb08";
+  // const MW_url = `https://dictionaryapi.com/api/v3/references/collegiate/json/${
+  //   req.params.word
+  // }?key=${MW_API_KEY}`;
+  // const MW_response = await fetch(MW_url);
+  // const MW_data = await MW_response.json();
 
-  const data = {
-    word: MW_data
-  };
+  // const data = {
+  //   word: MW_data
+  // };
 
-  res.json(data);
+  // res.json(data);
+
+  const photo_response = await unsplash.search.photos(req.params.word, 1, 1);
+  const photos = await photo_response.json();
+  const url = photos.results[0].urls.small;
+  console.log(url);
+
+  const url_data = await fetch(url);
+
+  const img = await url_data.blob();
+
+  res.contentType("image/jpeg");
+  res.send(img);
 });
 
 app.listen(3000, () => {
